7badaa5430e4d1116e8f4345107d8fd7aa7fec38
SamarthBM <samarthbm18@gmail.com>
2021-10-29 09:04:24 +0530

    [Samarth] Refactor: added topic name in .env file

--- StockData/consumer.py
+++ StockData/consumer.py
@@ -1,13 +1,15 @@
 from kafka import KafkaConsumer
 from pydoop import hdfs
+import os
+import json
 
+TOPIC_NAME = os.getenv('TOPIC_NAME')
 
-TOPIC_NAME = 'StockData'
-
-consumer = KafkaConsumer(TOPIC_NAME)
-hdfs_path = 'hdfs://localhost:9000//LiveStockData/data.txt'
+consumer = KafkaConsumer(TOPIC_NAME,auto_offset_reset = 'earliest',group_id = None,
+                     value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+hdfs_path = 'hdfs://localhost:9000//LiveStockData/data1.txt'
 
 for message in consumer:
     values = message.value
-    with hdfs.open(hdfs_path, 'a') as wfile:    
-        wfile.write(values)
+    with hdfs.open(hdfs_path, 'at') as f:
+        f.write(f"{values}\n")
